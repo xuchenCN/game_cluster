b6fba51358d3f383205bbda2a21efd99938085fe
xuchenCN <chenxu198511@gmail.com>
2016-07-04 10:42:32 +0800

    module create

---
+++ server-gate/src/main/java/org/server/gate/App.java
@@ -0,0 +1,13 @@
+package org.server.gate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
